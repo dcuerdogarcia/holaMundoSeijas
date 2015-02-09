c99d7ed56eab148cbb349695fefd9a694f2b64fb
dcuerdogarcia <dcuerdogarcia@eniac10.danielcastelao.org>
2015-02-09 09:23:49 +0100

    Commit seijas

---
+++ src/library/SeijasImbecil.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+/**
+ *
+ * @author dcuerdogarcia
+ */
+public class SeijasImbecil {
+    public static void seijasparvo(){
+            System.out.println("Hola, soy Joseba de Carglass");
+      
+}
+}
